52ef4a9cea46c6a9792c4debd20fc98c44c4270b
NetAcad DEVASC <changeme@example.com>
2023-10-28 05:15:33 +0000

    option2

--- option2.py
+++ option2.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 import requests
+import pdfkit
 
 app = Flask(__name__)
 
@@ -17,6 +18,21 @@ def get_ip_info():
 def index():
     ip_info = get_ip_info()
     return render_template('index.html', ip_info=ip_info)
+
+@app.route('/export_pdf')
+def export_pdf():
+    ip_info = get_ip_info()
+
+    if ip_info:
+        rendered_html = render_template('pdf.html', ip_info=ip_info)
+        config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
+        pdf = pdfkit.from_string(rendered_html, False, configuration=config)
+        response = make_response(pdf)
+        response.headers['Content-Disposition'] = 'attachment; filename=ip_info.pdf'
+        response.headers['Content-type'] = 'application/pdf'
+        return response
+
+    return "Failed to retrieve."
 
 if __name__ == "__main__":
     app.run (debug=True)
